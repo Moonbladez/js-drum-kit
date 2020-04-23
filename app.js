68af0d9f5b00cc054b43b07dedfb897acc37761d
Bekki Young <moonbladez@hotmail.com>
2020-04-23 21:22:52 +0200

    inital commit

---
+++ app.js
@@ -0,0 +1,24 @@
+function playSound(event) {
+	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+	if (!audio) {
+		return; //stop it from running all together
+	}
+	audio.currentTime = 0; //rewind to start
+	audio.play();
+	//add class playing
+	key.classList.add("playing");
+}
+
+function removeTransition(event) {
+	if (event.propertyName !== "transform") {
+		return; //skip if its not a transform
+	}
+	//remove transition
+	this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
